586b943cad34ed46b95cde4633204f3e273d7777
Elias Neto <netoeac.dev@gmail.com>
2023-12-24 08:03:44 -0300

    register page added

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,20 @@
+import RegisterForm from '../ui/register-form';
+import registerBg from '../assets/auth/register-bg.jpg';
+import Image from "next/image";
+
 export default function RegisterPage() {
   return (
-    < main>
-      <p>Register</p>
+    <main>
+      <div className="flex flex-row">
+        <div className="flex-auto relative" >
+          <div className="absolute inset-0 bg-blue-500 opacity-50"></div>
+          <Image className="object-cover h-screen" src={registerBg} alt="Descrição da imagem" />
+        </div>
+        <div className="w-1/2 min-w-96">
+
+          <RegisterForm />
+        </div>
+      </div>
     </main>
   );
 }
