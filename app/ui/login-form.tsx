7945fda033c4a7f53ebe983655f4b35899ad24e1
Elias Neto <netoeac.dev@gmail.com>
2023-12-24 09:01:48 -0300

    added link to dashboard from login

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,6 +1,7 @@
 
 import Image from 'next/image'
 import Logo from '../assets/auth/tt-logo.png'
+import Link from 'next/link'
 
 export default function LoginForm() {
   return (
@@ -69,12 +70,13 @@ export default function LoginForm() {
             </div>
 
             <div>
-              <button
-                type="submit"
+              <Link href="/dashboard"
+                // type="submit"
+
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Entrar
-              </button>
+              </Link>
             </div>
           </form>
 
